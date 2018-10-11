6b7a7f9c6fe59eff572cd5193b9f2278e4790e13
Pablo Novas <pjnovas@gmail.com>
2018-10-11 15:14:59 -0300

    added image procesor

--- scripts/parseImages.js
+++ scripts/parseImages.js
@@ -1,31 +1,60 @@
 let fs = require('fs-extra');
 let path = require('path');
 let jimp = require('jimp');
+let flatten = require('lodash/flatten');
+
+let logPer = total => curr => console.log(`Processing ${curr}/${total}`);
 
 async function main() {
-  let guideDir = path.resolve('./src/guide');
-  let staticDir = path.resolve('./static');
+  // TODO: move this props into params
+  const guideDir = path.resolve('./src/guide');
+  const staticDir = path.resolve('./static/guide');
+  const scale = 0.5;
+  const quality = 40;
 
-  let images = await fs.readdir(guideDir);
-  console.log(images);
+  // -----------
 
-/*
+  let files = await fs.readdir(guideDir);
+  let dirs = files.filter(filename => !filename.includes('.'));
 
-  Promise.all(
-
+  let images = await Promise.all(
+    dirs.map(dir => fs.readdir(`${guideDir}/${dir}`))
   )
+  .then(result => dirs.reduce((all, dir, i) => Object.assign(all, {
+    [dir]: result[i].filter(filename => (/\.(jpe?g|png)$/i).test(filename))
+  }), {}));
 
-  Jimp.read('lenna.png')
-    .then(lenna => {
-      return lenna
-        .scale(0.5)
-        .quality(60)
-        .write('lena-small-bw.jpg'); // save
-    })
-    .catch(err => {
-      console.error(err);
-    });
-*/
+  let log = logPer(flatten(Object.keys(images).map(dir => images[dir])).length);
+  let curr = 0;
+
+  console.time('process');
+  log(curr);
+
+  Promise.all(flatten(
+    Object.keys(images).map(dir =>
+      images[dir].map(img =>
+        jimp
+          .read(`${guideDir}/${dir}/${img}`)
+          .then(image => image
+            .scale(scale)
+            .quality(quality)
+            .writeAsync(`${staticDir}/${dir}/${img}`)
+            .then(data => {
+              log(++curr);
+              return data;
+            })
+          )
+      )
+    )
+  )).then(result => {
+    console.log(`ALL DONE ${result.length}`)
+  })
+  .catch(e => {
+    console.log(e)
+  })
+  .then(() => {
+    console.timeEnd('process');
+  })
 }
 
 main();
